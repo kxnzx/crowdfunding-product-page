09e65d73ea50a085e0cddfd549e983187d83b8af
“kxnzx” <k.bousboa@gmail.com>
2023-10-28 14:54:37 +0200

    edit

---
+++ script.js
@@ -0,0 +1,27 @@
+/*const box = document.getElementById("box");
+let radioBtns = document.getElementsByTagName("input");
+
+function handleRadioClick() {
+  if (document.getElementByTagName("show").checked) {
+    box.style.display = "block";
+  } else {
+    property.radio.checked = false;
+  }
+}
+
+const radioButtons = document.querySelectorAll('input[name="example"]');
+radioButtons.forEach((radio) => {
+  radio.addEventListener("click", handleRadioClick);
+});
+*/
+
+/*
+let radios = document.getElementsByTagName("input");
+for (i = 0; i < radios.length; i++) {
+  radios[i].onclick = function (e) {
+    if (e.ctrlKey) {
+      this.checked = false;
+    }
+  };
+}
+*/
