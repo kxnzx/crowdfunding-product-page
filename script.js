b99c264da89ecd451a315b195e6164dc877bc0b4
“kxnzx” <k.bousboa@gmail.com>
2023-10-30 17:16:52 +0100

    edit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// Back this Project Button
+// Using strict mode helps you to catch and identify certain errors
+"use strict";
 
 const pL_button = document.getElementById("pledgeList_button");
+const close_pL = document.getElementById("close_pledgeList");
+const radioButtons = document.querySelectorAll("input");
 
 function display_pledgeList() {
   document.getElementById("pledgeList").style.display = "block";
@@ -8,6 +11,25 @@ function display_pledgeList() {
 
 // Call the function display_pledgeList() when pledgeList_button is clicked
 pL_button.onclick = display_pledgeList;
+
+function close_pledgeList() {
+  document.getElementById("pledgeList").style.display = "none";
+}
+
+// Call the function close_pledgeList() when close_pledgeList button is clicked
+close_pL.onclick = close_pledgeList;
+
+radioButtons.forEach((radio) => {
+  radio.addEventListener("click", handleRadioClick);
+});
+
+function handleRadioClick() {
+  if (document.getElementByTagName("input").checked) {
+    form.style.display = "block";
+  } else {
+    property.radio.checked = false;
+  }
+}
 
 /*const box = document.getElementById("box");
 let radioBtns = document.getElementsByTagName("input");
