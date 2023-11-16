2653c11a9cc5f7ffdf920c09d9bab4262d4688e7
“kxnzx” <k.bousboa@gmail.com>
2023-11-16 16:23:52 +0100

    edit

--- script.js
+++ script.js
@@ -351,30 +351,11 @@ form3.addEventListener("submit", handleSubmit3);
 form4.addEventListener("submit", handleSubmit4);
 
 // Select Reward Buttons 
-const selectBtn1 = document.getElementById("selectBtn1");
-const selectBtn2 = document.getElementById("selectBtn2");
-const selectBtn3 = document.getElementById("selectBtn3");
+const selectBtns = document.querySelectorAll(".selectBtn");
+//console.log(selectBtns);
 
-/*
-
-const section1 = document.getElementById("section1");
-
-// Display list with Radio Buttons
-function display_section1() {
-  document.getElementById("section1").style.display = "block";
-}
-
-selectBtn1.onclick = display_section1;
-
-
-// Display list with Radio Buttons
-function display_pledgeList() {
-  document.getElementById("pledgeList").style.display = "block";
-}
-
-// Call the function display_pledgeList() when pledgeList_button is clicked
-pL_button.onclick = display_pledgeList;
-
-
-
-*/
+// Display Pledgelist 
+selectBtns.forEach((btn)=>{
+  btn.onclick = display_pledgeList;
+});
+  
